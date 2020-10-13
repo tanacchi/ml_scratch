adf3807280c3ce169e116efc8b902872c055156a
tanacchi <q111026d@mail.kyutech.jp>
2020-10-13 14:18:01 +0900

    Fix dumb runtime error

--- UKR-LP/data.py
+++ UKR-LP/data.py
@@ -11,6 +11,6 @@ def gen_saddle_shape(resolution, random_seed=None, noise_scale=0.1):
     X[:, 0] = z1
     X[:, 1] = z2
     X[:, 2] = z1**2 - z2**2
-    X += np.random.normal(loc=0, scale=noise_scale, shape=X.shape)
+    X += np.random.normal(loc=0, scale=noise_scale, size=X.shape)
 
     return X
--- UKR-LP/ukr.py
+++ UKR-LP/ukr.py
@@ -4,7 +4,7 @@ from scipy.spatial import distance as dist
 from collections import namedtuple
 
 
-History = ('History', ['Y', 'f', 'Z'])
+History = namedtuple('History', ['Y', 'f', 'Z'])
 
 class UKR(object):
     def __init__(self, latent_dim, eta):
@@ -17,16 +17,16 @@ class UKR(object):
         N, D = X.shape
         f_resolution = 10
         Z = np.random.normal(scale=0.1, size=(N, self.L))
-        history = History(Y=np.empty((num_epoch, N, D)),
-                          f=np.empty((num_epoch, f_resolution**self.L, D)),
-                          Z=np.empty((num_epoch, N, self.L)))
+        history = History(np.zeros((num_epoch, N, D)),
+                          np.zeros((num_epoch, f_resolution**self.L, D)),
+                          np.zeros((num_epoch, N, self.L)))
 
         for epoch in tqdm(range(num_epoch)):
             Y, R = self.estimate_f(X, Z)
             Z = self.estimate_e(X, Y, Z, R)
 
             Z_new = make_grid2d(f_resolution, bounds=(np.min(Z), np.max(Z)))
-            f = self.estimate_f(X, Z_new, Z)
+            f, _ = self.estimate_f(X, Z_new, Z)
 
             history.Y[epoch] = Y
             history.f[epoch] = f
@@ -35,14 +35,14 @@ class UKR(object):
         return history
 
     def estimate_f(self, X, Z1, Z2=None):
-        Z2 = np.copy(Z1) if not Z2 else Z2
+        Z2 = np.copy(Z1) if Z2 is None else Z2
         kernels = self.kernel(Z1, Z2)
         R = kernels / np.sum(kernels, axis=1, keepdims=True)
         return R @ X, R
 
-    def estimate_e(X, Y, Z, R):
+    def estimate_e(self, X, Y, Z, R):
         d_ii = Y - X
-        d_in = Y[:, np.newaxis, :] - X[:, :, np.newaxis]
+        d_in = Y[:, np.newaxis, :] - X[np.newaxis, :, :]
         d_ni = Y[np.newaxis, :, :] - X[:, np.newaxis, :]
         δ_ni = Z[np.newaxis, :, :] - Z[:, np.newaxis, :]
         δ_in = Z[:, np.newaxis, :] - Z[np.newaxis, :, :]
