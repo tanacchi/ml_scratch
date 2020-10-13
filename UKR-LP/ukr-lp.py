3846ad61b8470ca56c2d57bb6d4c310970f2092d
tanacchi <q111026d@mail.kyutech.jp>
2020-10-13 19:34:55 +0900

    Upgrade UKR-LP

--- UKR-LP/ukr-lp.py
+++ UKR-LP/ukr-lp.py
@@ -1,26 +1,31 @@
 import numpy as np
 from tqdm import tqdm
-from scipy.spatial import distance as dist
 from collections import namedtuple
+import torch
 
 
-History = namedtuple('History', ['F', 'f', 'Z'])
+History = namedtuple('History', ['Y', 'F', 'f', 'Zeta', 'Z'])
 
 class UKR(object):
     def __init__(self, latent_dim, eta):
         self.L = latent_dim
         self.η = eta
         self.σ = 1
-        self.kernel = lambda Z1, Z2: np.exp(- dist.cdist(Z1, Z2)**2 / (2 * self.σ ** 2))
+        self.kernel = lambda Z1, Z2: torch.exp(- torch.cdist(Z1, Z2)**2 / (2 * self.σ ** 2))
 
     def fit(self, X, num_epoch=50):
         N, D = X.shape
+        X = torch.from_numpy(X).float()
         landmarks_resolution = 5
+        K = landmarks_resolution**2
         f_resolution = 10
-        Z = np.random.normal(scale=0.1, size=(N, self.L))
+        Z = torch.normal(mean=0.0, std=0.1, size=(N, self.L))
+        Z.requires_grad = True
         Zeta = make_grid2d(landmarks_resolution, (-1, 1))
-        history = History(np.zeros((num_epoch, N, D)),
+        history = History(np.zeros((num_epoch, K, D)),
+                          np.zeros((num_epoch, N, D)),
                           np.zeros((num_epoch, f_resolution**self.L, D)),
+                          np.zeros((num_epoch, K, self.L)),
                           np.zeros((num_epoch, N, self.L)))
 
         for epoch in tqdm(range(num_epoch)):
@@ -28,22 +33,22 @@ class UKR(object):
             F = self.estimate_f(X, Z)
             Z = self.estimate_e(X, F, Z, Zeta)
 
-            #  Z_new = make_grid2d(f_resolution, bounds=(np.min(Z), np.max(Z)))
-            #  f, _ = self.estimate_f(X, Z_new, Z)
+            Z_new = make_grid2d(f_resolution, bounds=(torch.min(Z.detach()), torch.max(Z.detach())))
+            f = self.estimate_f(X, Z_new, Z)
 
-            history.F[epoch] = F
-            history.f[epoch] = f
-            history.Z[epoch] = Z
+            history.Y[epoch] = Y.detach().numpy()
+            history.F[epoch] = F.detach().numpy()
+            history.f[epoch] = f.detach().numpy()
+            history.Zeta[epoch] = Zeta.numpy()
+            history.Z[epoch] = Z.detach().numpy()
 
         return history
 
     def estimate_lp(self, X, Z, Zeta):
-        N, _ = X.shape
-
         g_ik = self.kernel(Z, Zeta)
-        G_i = np.sum(g_ik, axis=1)
+        G_i = torch.sum(g_ik, axis=1)
         p_ki = g_ik.T / G_i
-        P_k = np.sum(p_ki, axis=1)
+        P_k = torch.sum(p_ki, axis=1)
         q_ik = p_ki.T / P_k
         return q_ik.T @ X, P_k
 
@@ -54,24 +59,18 @@ class UKR(object):
         return F
 
     def estimate_f(self, X, Z1, Z2=None):
-        Z2 = np.copy(Z1) if Z2 is None else Z2
+        Z2 = Z1.clone() if Z2 is None else Z2
         kernels = self.kernel(Z1, Z2)
-        R = kernels / np.sum(kernels, axis=1, keepdims=True)
+        R = kernels / torch.sum(kernels, axis=1, keepdims=True)
         return R @ X
 
     def estimate_e(self, X, F, Z, Zeta):
-        d_ii = F - X
-        d_in = F[:, np.newaxis, :] - X[np.newaxis, :, :]
-        d_ni = F[np.newaxis, :, :] - X[:, np.newaxis, :]
-        δ_ni = Z[np.newaxis, :, :] - Z[:, np.newaxis, :]
-        δ_in = Z[:, np.newaxis, :] - Z[np.newaxis, :, :]
-        kernels = self.kernel(Z, Zeta)
-        R = kernels / np.sum(kernels, axis=1, keepdims=True)
-
-        diff_left = np.einsum("ni,nd,ind,inl->nl", R, d_ii, d_ni, δ_ni)
-        diff_right = np.einsum("ni,id,ind,inl->nl", R.T, d_ii, d_in, δ_in)
-        diff = 2 * (diff_left - diff_right) / X.shape[0]
-        return Z - self.η * diff
+        E = torch.sum((F - X)**2) / 2.0
+        E.backward()
+        Z.requires_grad = False
+        Z = Z - self.η * Z.grad
+        Z.requires_grad = True
+        return Z
 
 
 def make_grid2d(resolution, bounds=(-1, +1)):
@@ -79,14 +78,16 @@ def make_grid2d(resolution, bounds=(-1, +1)):
                              endpoint=False, retstep=True)
     mesh += step / 2.0
     grid = np.meshgrid(mesh, mesh)
-    return np.dstack(grid).reshape(-1, 2)
+    Zeta = np.dstack(grid).reshape(-1, 2)
+    return torch.from_numpy(Zeta).float()
 
 
 if __name__ == '__main__':
+    import sys
     from data import gen_saddle_shape
-    from visualizer import visualize_history
+    from visualizer_lp import visualize_history
 
     X = gen_saddle_shape(100, noise_scale=0.0)
-    ukr = UKR(latent_dim=2, eta=100)
+    ukr = UKR(latent_dim=2, eta=0.8)
     history = ukr.fit(X, num_epoch=50)
-    visualize_history(X, history.f, history.Z)
+    visualize_history(X, history, save_gif=(len(sys.argv) == 2))
