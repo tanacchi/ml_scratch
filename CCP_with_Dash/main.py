22e50e625ff14c991010c959f4b9089071c56980
tanacchi <q111026d@mail.kyutech.jp>
2020-10-16 19:25:56 +0900

    Implement ConditionalCompoentPlane with Dash

--- CCP_with_Dash/main.py
+++ CCP_with_Dash/main.py
@@ -2,6 +2,7 @@ import dash
 import dash_core_components as dcc
 import dash_html_components as html
 import numpy as np
+from scipy.spatial import distance as dist
 import pandas as pd
 from dash.dependencies import Input, Output
 import plotly.express as px
@@ -31,57 +32,43 @@ app.layout = html.Div([
         dcc.Graph(id='mode2'),
         className='four columns'
     ),
-    html.Div(
-        dcc.Input(id='tmp-input', type='text')
-    )
 ], className='row')
 
-def get_figure(Zeta):
-    #  if selectedpoints_local and selectedpoints_local['range']:
-        #  ranges = selectedpoints_local['range']
-        #  selection_bounds = {'x0': ranges['x'][0], 'x1': ranges['x'][1],
-                            #  'y0': ranges['y'][0], 'y1': ranges['y'][1]}
-    #  else:
-        #  selection_bounds = {'x0': np.min(df[x_col]), 'x1': np.max(df[x_col]),
-                            #  'y0': np.min(df[y_col]), 'y1': np.max(df[y_col])}
 
-    fig = px.scatter(x=Zeta[:, 0], y=Zeta[:, 1])
-
-    #  fig.update_traces(selectedpoints=selectedpoints,
-                      #  customdata=df.index,
-                      #  mode='markers+text', marker={ 'color': 'rgba(0, 116, 217, 0.7)', 'size': 20 }, unselected={'marker': { 'opacity': 0.3 }, 'textfont': { 'color': 'rgba(0, 0, 0, 0)' }})
-
-    #  fig.update_layout(margin={'l': 20, 'r': 0, 'b': 15, 't': 5}, dragmode='select', hovermode=False)
-
-    #  fig.add_shape(dict({'type': 'rect',
-                        #  'line': { 'width': 1, 'dash': 'dot', 'color': 'darkgrey' }},
-                       #  **selection_bounds))
+def get_figure(Zeta, cp):
+    fig = px.imshow(cp)
     return fig
 
 
 @app.callback(
     Output('mode1', 'figure'),
-    [Input(component_id='tmp-input', component_property='value')]
+    [Input(component_id='mode2', component_property='clickData')]
 )
-def update_mode1_latent(value):
-    #  selectedpoints = df.index
-    #  for selected_data in [selection1, selection2, selection3]:
-        #  if selected_data and selected_data['points']:
-            #  selectedpoints = np.intersect1d(selectedpoints,
-                #  [p['customdata'] for p in selected_data['points']])
-
-    #  return [get_figure(df, "Col 1", "Col 2", selectedpoints, selection1),
-            #  get_figure(df, "Col 3", "Col 4", selectedpoints, selection2),
-            #  get_figure(df, "Col 5", "Col 6", selectedpoints, selection3)]
-    return get_figure(Zeta1)
+def update_mode1_latent(clickData):
+    clicked_unit = get_bmu(Zeta1, clickData)
+    tmp = history.Y[-1, :, clicked_unit, :]
+    component_plane = np.sqrt(np.sum(tmp * tmp, axis=1)).reshape(10, 10)
+    return get_figure(Zeta1, cp=component_plane)
 
 
 @app.callback(
     Output('mode2', 'figure'),
-    [Input(component_id='tmp-input', component_property='value')]
+    [Input(component_id='mode1', component_property='clickData')]
 )
-def update_mode2_latent(value):
-    return get_figure(Zeta2)
+def update_mode2_latent(clickData):
+    print(clickData)
+    clicked_unit = get_bmu(Zeta2, clickData)
+    tmp = history.Y[-1, clicked_unit, :, :]
+    component_plane = np.sqrt(np.sum(tmp * tmp, axis=1)).reshape(10, 10)
+    return get_figure(Zeta2, cp=component_plane)
+
+
+def get_bmu(Zeta, clickData):
+    clicked_point = [[clickData['points'][0]['x'], clickData['points'][0]['y']]] if clickData else [[0, 0]]
+    clicked_point = np.array(clicked_point)
+    dists = dist.cdist(Zeta, clicked_point)
+    unit = np.argmin(dists, axis=0)
+    return unit[0]
 
 
 if __name__ == '__main__':
